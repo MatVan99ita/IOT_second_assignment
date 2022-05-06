0b10d82acfeabdd25a8943af0093301faa26fefa
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-05-06 10:59:20 +0200

    Added component and utility lib

--- Assignment 2/Macchinetta_bevande/Macchinetta_bevande.ino
+++ Assignment 2/Macchinetta_bevande/Macchinetta_bevande.ino
@@ -1,3 +1,9 @@
+#include <Servo.h>
+#include <LiquidCrystal.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <EnableInterrupt.h>
+#include "TimerOne.h"
 
 /*Librerie con strumenti di utilità*/
 #include "UtilityLib/counter.h"
@@ -9,9 +15,49 @@
 #include "ComponentLib/lm35_impl.h"
 #include "ComponentLib/ServoTimer2.h"
 
+Servo myservo;
+int zucchero;
+long int sleepTimer;
+int funzionamento = 0;
 
-void setup(){
+
+void Avvio(){
+  
+  //lcd.begin(16, 2);
+lcd.print("Benvenuto, seleziona la tua bevanda");
+delay(5000);
+lcd.clear();
+
+  }
+
+void Zucchero(){
+  int newValue = analogRead(POT_PIN);
+  zucchero = newValue/256 ; //assegno un valore tra 0 e 3
+  }
+
+void Creazione(){
+  myservo.write(0);
+  delay(1500)     //va cambiato rispetto al tempo di creazione
+  myservo.write(180);         //ho finito di creare
+  serial.println("La bevanda e' pronta"); //deve essere scritta sul display
+  }
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+sleepTimer = 10000000;
+Timer1.initialize();
+Avvio();
 }
 
-void loop(){
+void loop() {
+  // put your main code here, to run repeatedly:
+
+switch(fuhnzionamento)
+case 0://sleep
+
+set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+sleep_enable();
+sleep_mode();
+
 }
