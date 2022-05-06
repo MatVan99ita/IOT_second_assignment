0103df4c42919c6f789afbbf3c6465a9318441aa
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-05-06 10:55:23 +0200

    Added component and utility library

--- Assignment 2/Macchinetta_bevande/Macchinetta_bevande.ino
+++ Assignment 2/Macchinetta_bevande/Macchinetta_bevande.ino
@@ -1,4 +1,15 @@
 
+/*Librerie con strumenti di utilità*/
+#include "UtilityLib/counter.h"
+#include "UtilityLib/MsgService.h"
+#include "UtilityLib/Timer.h"
+
+/*Librerie per le componenti arduino*/
+#include "ComponentLib/servo_motor_impl.h"
+#include "ComponentLib/lm35_impl.h"
+#include "ComponentLib/ServoTimer2.h"
+
+
 void setup(){
 }
 
