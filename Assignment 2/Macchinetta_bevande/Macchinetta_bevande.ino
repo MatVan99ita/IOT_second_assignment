a3d726c6569ea52e8592bf378f0be92c6ff8d92a
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-05-11 16:28:36 +0200

    Settati valori

--- Assignment 2/Macchinetta_bevande/Macchinetta_bevande.ino
+++ Assignment 2/Macchinetta_bevande/Macchinetta_bevande.ino
@@ -13,11 +13,11 @@ void setup(){
   Serial.begin(9600);
   scheduler->init(100);
   Task* t0 = new TaskMake(servoPin);
-  t0 -> init(100);
+  t0 -> init(4000);
   Task* t1 = new TaskAssistance("Refill");
-  t1 -> init(500);
+  t1 -> init(50000);
   Task* t2 = new TaskTest();
-  t1 -> init(1800);
+  t1 -> init(18000);
   scheduler->addTask(t0);
   scheduler->addTask(t1);
   scheduler->addTask(t2);
