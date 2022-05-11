a60a7556d2ba0bcec4f66fa42843d3be70c4f3ea
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-05-11 10:25:19 +0200

    Altri task

--- Assignment 2/Macchinetta_bevande/TaskMake.h
+++ Assignment 2/Macchinetta_bevande/TaskMake.h
@@ -5,35 +5,37 @@
 #include "bevandaImpl.h"
 #include "TaskTest.h"
 
-#define BEVERAGE_QUANTITY_TEST 3
+#define BEVERAGE_QUANTITY_TEST 1
 
 class Task_Make {
     
 public: 
 
-  void init(){
-    this->status = "Welcome";
-    this->Chocolate = new BevandaImpl(BEVERAGE_QUANTITY_TEST);
-    this->Tea->setQuantity(BEVERAGE_QUANTITY_TEST);
-    this->Coffee->setQuantity(BEVERAGE_QUANTITY_TEST);
-  }  
+    void init(){
+        this->status = "Welcome";
+        this->Chocolate =->setQuantity(BEVERAGE_QUANTITY_TEST);
+        this->Tea->setQuantity(BEVERAGE_QUANTITY_TEST);
+        this->Coffee->setQuantity(BEVERAGE_QUANTITY_TEST);
+        this->test->setSelfTest(0);
+    }
 
-  bool isMsgAvailable();
-  Msg* receiveMsg();
 
-  bool isMsgAvailable(Pattern& pattern);
-  Msg* receiveMsg(Pattern& pattern);
-  
-  void sendMsg(const Msg& msg);
-  void sendMsg(const String& msg);
+    bool isMsgAvailable();
+    Msg* receiveMsg();
+
+    bool isMsgAvailable(Pattern& pattern);
+    Msg* receiveMsg(Pattern& pattern);
+    
+    void sendMsg(const Msg& msg);
+    void sendMsg(const String& msg);
 
 private:
-  BevandaImpl Chocolate;
-  BevandaImpl Tea;
-  BevandaImpl Coffee;
-  String status;
-  int PrimoAvvio = 0;
-
+    BevandaImpl* Chocolate;
+    BevandaImpl* Tea;
+    BevandaImpl* Coffee;
+    String status;
+    int PrimoAvvio = 0;
+    TaskTest* test;
 };
 
 extern MsgServiceClass MsgService;
--- Assignment 2/Macchinetta_bevande/TaskTest.h
+++ Assignment 2/Macchinetta_bevande/TaskTest.h
@@ -1,1 +1,35 @@
+#ifndef __TASKTEST__
+#define __TASKTEST__
 
+
+#include "Arduino.h"
+#include "ServoTimer2.h"
+#include "lm35_impl.h"
+
+class TaskTest
+{
+public:
+    TaskTest(){
+        this->self_test = 0;
+    }
+
+    void setSelfTest(int test){
+        this->self_test = test;
+    }
+
+    int getSelfTest(){
+        return this->self_test;
+    }
+
+    void makeTest()
+
+private:
+    int self_test;
+    ServoTimer2* servo;
+    LM35* lm35;
+};
+
+void TaskTest::makeTest(){
+}
+
+#endif
